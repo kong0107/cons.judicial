5c259b288ee7a7e245b7a2646066a4adefb10402
kong0107 <kong0107@gmail.com>
2022-11-01 00:37:46 +0800

    debug

--- grounds.js
+++ grounds.js
@@ -7,15 +7,17 @@ import toCSV from './lib/toCSV.mjs';
 await fs.mkdir('./source/40', {recursive: true});
 const summary = [];
 
+let hasNew;
 for(let href = '/docdata.aspx?fid=40&page=1', counter = 0; href;) {
     const filepath = './source/' + encodeURIComponent(href.split('/').pop()) + '.html';
-    await download('https://cons.judicial.gov.tw' + href, filepath);
+    if(!counter || hasNew)
+        await download('https://cons.judicial.gov.tw' + href, filepath);
 
     const html = await fs.readFile(filepath);
     const document = (new JSDOM(html)).window.document;
     const anchors = document.querySelectorAll('[href^="/docdata.aspx?fid=40&id="]');
 
-    let hasNew = false;
+    hasNew = false;
     for(let anchor of anchors) {
         const id = (new URLSearchParams(anchor.href)).get('id');
         const [, year, word, number] = anchor.textContent.match(/^(\d+)年度?(.+)字第(\d+)號(\(|$)/); // exception: 339927
@@ -45,10 +47,6 @@ for(let href = '/docdata.aspx?fid=40&page=1', counter = 0; href;) {
         await fs.mkdir(`./docket/${year}/${word}/`, {recursive: true});
         await fs.writeFile(`./docket/${year}/${word}/${number}.json`, JSON.stringify(data, null, '\t'));
         if(!(++counter % 50)) process.stdout.write('.');
-    }
-    if(!hasNew) {
-        process.stdout.write('\nIt seems there is no new decisions.');
-        break;
     }
     href = document.querySelector('[id$=paging_next]')?.href;
 }
